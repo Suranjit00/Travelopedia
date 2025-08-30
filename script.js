a24652759795b8307775a756acb3caba0a3aea68
Suranjit00 <suranjitsaha593@gmail.com>
2025-08-31 00:03:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Show/Hide Password
+function togglePassword(inputId, btn) 
+{
+  const input = document.getElementById(inputId);
+  const isPassword = input.type === 'password';
+  input.type = isPassword ? 'text' : 'password';
+  btn.textContent = isPassword ? '🙈' : '🐵';
+}
+
+// Popup show on load
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("popupModal").style.display = "flex";
+});
+
+// Close popup
+function closePopup() 
+{
+  document.getElementById("popupModal").style.display = "none";
+}
+
+// Date & Time
+  function updateDateTime() 
+  {
+    const now = new Date();
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric', 
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    };
+    document.getElementById("datetime").textContent = now.toLocaleString("en-US", options);
+  }
+  updateDateTime();
+  setInterval(updateDateTime, 30000);
